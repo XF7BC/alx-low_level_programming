a35b5db3b047e85193e9513410db70aa3fd02e3f
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-15 04:13:00 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * main - main block
- * Description: Using 'putchar' to print alphabets
- * Return: 0
- */
-int main(void)
-{
-	char x ='a';
+#include <stdio.h> 
+#include <stdlib.h> 
+/** 
+ * main - main block 
+ * Description: Using 'putchar' to print alphabets 
+ * Return: 0 
+ */ 
+int main(void) 
+{ 
+	char x = 'a'; 
 
-	while (x <= 'z')
-	{
-		putchar(x);
-		x++;
-	}
-	putchar('\n');
-	return (0);
+	while (x <= 'z') 
+	{ 
+		putchar(x); 
+		x++; 
+	} 
+	putchar('\n'); 
+	return (0); 
 }
