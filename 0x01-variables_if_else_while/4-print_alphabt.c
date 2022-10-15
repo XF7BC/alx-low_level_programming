dab2511df6a3e1688a3bb64a714dfe0029f185d0
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-15 04:38:42 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Using 'purchar' to print all lowercase except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
