39398da1a768c95f3695195123b1859a2cfd679e
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-19 00:08:51 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - prints the alphabets 10 times,
  * in lowercase, followed by a new line
- * Return: 0
+ * Return: 0 on success
  */
 
 void print_alphabet_x10(void)
@@ -10,14 +10,14 @@ void print_alphabet_x10(void)
 	int a = 97;
 	int i = 1;
 
-	while (1 <= 10)
+	while (i <= 10)
 	{
 		while (a <= 122)
 		{
 			_putchar(a);
 			a++;
 		}
-
+		
 		_putchar('\n');
 		i++;
 	}
