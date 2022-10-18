81390ce332177e8d5dbe45671ae6a9040fbbcfae
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-19 00:54:07 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: Unknown integer
+ *
+ * Return: absolute of n if successful
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+  else
+	{
+		return (-1 * n);
+	}
+}
