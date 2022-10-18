bc8187be52234a75e4d793c2e44149fd47823437
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-18 22:51:58 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,13 @@
 
 int print_alphabet(void)
 {
-  char c = 'a';
-  while (c <= 'z')
-  {
-    _putchar(c);
-    c++;
-  }
-  _putchar('\n');
-  return (0);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
 }
