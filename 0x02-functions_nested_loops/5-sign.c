2aecdffb3f1ecf408d2e02d6b8034bc8c474e891
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-19 00:51:18 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,10 +2,10 @@
 
 /**
  * print_sign - function to print the sign of a number
- * @n: number to check for sign
+ * @n: Unknown integer
  *
- * Description: prints + if n is greater than zero, 0 if n is zero,
- * and - if n is less than zero
+ * Description: prints '+' if n is greater than zero, '0' if n is zero,
+ * and '-' if n is less than zero
  *
  * Return: 1 if positive, 0 if zero, and -1 if negative
  */
@@ -15,18 +15,18 @@ int print_sign(int n)
 	if (n == 0)
 	{
 		_putchar('0');
-		
+
 		return (0);
 
 	}
-  else if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 
 		return (-1);
 
 	}
-  else
+	else
 	{
 		_putchar('+');
 
