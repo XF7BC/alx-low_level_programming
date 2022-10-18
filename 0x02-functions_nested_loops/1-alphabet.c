f7bf8d59b36eb0b8a2dd3449d252145e5e3b5cb5
Ajibola Abiola <103952064+XF7BC@users.noreply.github.com>
2022-10-18 23:29:30 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - main - body
+ * print_alphabet - prints alphabet out in lowercase using _putchar
  * Description: Prints alphabets in lowercase,
  * followed by a new line
  * Return: 0
@@ -8,6 +8,7 @@
 void print_alphabet(void)
 {
 	int a = 97;
+
 	while (a <= 122)
 	{
 		_putchar(a);
